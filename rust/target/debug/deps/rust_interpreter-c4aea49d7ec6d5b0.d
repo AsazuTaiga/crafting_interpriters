c38d4fefdca113b69845fea8f05788d95c759e50
poteboy <poteboy.dev@gmail.com>
2023-12-06 03:15:08 +0900

    ignore target

--- rust/target/debug/deps/rust_interpreter-c4aea49d7ec6d5b0.d
+++
@@ -1,7 +0,0 @@
-/Users/keitafuruse/development/rust-interpreter/crafting_interpriters/rust/target/debug/deps/rust_interpreter-c4aea49d7ec6d5b0: src/main.rs src/scanner.rs src/token.rs
-
-/Users/keitafuruse/development/rust-interpreter/crafting_interpriters/rust/target/debug/deps/rust_interpreter-c4aea49d7ec6d5b0.d: src/main.rs src/scanner.rs src/token.rs
-
-src/main.rs:
-src/scanner.rs:
-src/token.rs:
--- rust/target/debug/rust-interpreter.d
+++
@@ -1,1 +0,0 @@
-/Users/keitafuruse/development/rust-interpreter/target/debug/rust-interpreter: /Users/keitafuruse/development/rust-interpreter/src/main.rs
