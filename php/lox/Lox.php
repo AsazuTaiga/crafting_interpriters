1413046f703b102cc3e303ca740e33309111cce6
yodakaEngineer <engineeryodaka@gmail.com>
2024-01-04 23:00:02 +0900

    4の最初まで

--- php/lox/Lox.php
+++ php/lox/Lox.php
@@ -4,8 +4,48 @@ declare(strict_types=1);
 
 class Lox
 {
-    public static function main(): void
+    /**
+     * @param string[] $args
+     * @return void
+     */
+    public static function main(
+        array $args
+    ): void
     {
-        echo "Hello, world!";
+        if (count($args) > 1) {
+            echo ('Usage: jlox [script]');
+            exit(64);
+        } else if (count($args) == 1) {
+            self::runFile($args[0]);
+        } else {
+            self::runPrompt();
+        }
+    }
+
+    private static function runFile(string $path): void
+    {
+        self::run(file_get_contents($path));
+    }
+
+    private static function runPrompt()
+    {
+        $input = STDIN;
+
+        for (;;) {
+            echo '> ';
+            $line = fgets($input);
+            if ($line === false) break;
+            self::run($line);
+        }
+    }
+
+    private static function run(string $source): void
+    {
+        $scanner = new Scanner($source);
+        $tokens = $scanner->scanTokens();
+
+        foreach ($tokens as $token) {
+            echo $token;
+        }
     }
 }
---
+++ php/lox/Scanner.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+class Scanner
+{
+    public function __construct(string $source){}
+
+    /**
+     * @return string[]
+     */
+    public function scanTokens(): array {
+        return [];
+    }
+}
