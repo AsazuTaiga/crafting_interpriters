f5b761b6e8a8f7b5e8823a397de3e0327c0486c2
yodakaEngineer <engineeryodaka@gmail.com>
2024-01-04 23:46:03 +0900

    4.2まで

---
+++ php/lox/Token.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+class Token
+{
+
+    public function __construct(
+        private TokenType $type,
+        private string $lexeme,
+        private object $literal,
+        private int $line
+    ){}
+
+    public function __toString(): string
+    {
+        return "$this->type $this->lexeme $this->literal";
+    }
+}
